1d8d1fa8a19a73ffc44bfcbc96959e62d3ef46e6
AiluMulti <ailenlmrn@gmail.com>
2025-12-14 02:34:30 -0300

    Delete proyectos/personajes/personaje-mary/mary.js

--- proyectos/personajes/personaje-mary/mary.js
+++
@@ -1,4 +0,0 @@
-const menuSticky = document.getElementById("menu");
-        window.addEventListener("scroll", () => {
-            menuSticky.classList.toggle("scrolled", window.pageYOffset > 13);
-        });
