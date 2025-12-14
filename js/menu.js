731a4a4b40a005c4454ce7c0dfb26607e760253c
AiluMulti <ailenlmrn@gmail.com>
2025-12-14 01:24:36 -0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -6,12 +6,6 @@ const menuSticky = document.getElementById("menu");
             menuSticky.classList.toggle("scrolled", window.pageYOffset > 13);
         });
 
-        document.querySelectorAll(".borde").forEach(item => {
-            item.addEventListener("click", () => {
-                const audio = new Audio("../audio/sfx.mp3");
-                audio.play();
-            });
-        });
 
 
 
